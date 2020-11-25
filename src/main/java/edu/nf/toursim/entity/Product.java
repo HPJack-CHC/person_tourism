52c2b91bd983587608c49a356e53dd7d216c0197
HPJack“git commit -m 'commit hello.txt'vim a.txtvim b.txtgit add .git statusgit config --global user.email 978295592@qq.comgit config --global user.name HPJack“ <978295592@qq.com>
2020-11-25 16:04:49 +0800

    update Product.java

--- src/main/java/edu/nf/toursim/entity/Product.java
+++ src/main/java/edu/nf/toursim/entity/Product.java
@@ -1,10 +1,12 @@
 package edu.nf.toursim.entity;
 
+import java.math.BigDecimal;
+
 /**
  * @author HPJack
  * @date 2020/11/25 - 15:59
  */
 public class Product {
     private String name;
-    private double price;
+    private BigDecimal price;
 }
